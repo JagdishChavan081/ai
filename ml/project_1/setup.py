f5420c070c3617153fc4ee4fd6425c669894a0f1
JagdishChavan081 <jagdish.chavan081@gmail.com>
2023-05-20 16:14:00 +0530

    setup.py

--- ml/project_1/setup.py
+++ ml/project_1/setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-    name='mlproject',
+    name='project_1',
     version='0.0.1',
     author='Jagdish Chavan',
     author_email='jagdish.chavan080@gmail.com',
