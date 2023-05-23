64a6213e6804a77e570ad9e4233e5a6192c0d52b
JagdishChavan081 <jagdish.chavan081@gmail.com>
2023-05-23 14:18:43 +0530

    update

---
+++ ml/project_1/src/pipeline/predict_pipeline.py
@@ -0,0 +1,19 @@
+import sys
+import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass 
+
+class CustomeData:
+    def __init__(self,
+                 gender: str,
+                race_ethnicity:int
+                parental_level_of_education:
+                lunch : having lunch before test (standard or free/reduced)
+* test preparation course : complete or not complete before test
+* math score
+* reading score
+* writing score):
